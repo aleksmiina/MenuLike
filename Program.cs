937ed220a82bccea8962d598e57939ba9080d72b
aleksmiina <aleks.miina@gmail.com>
2025-02-10 09:42:59 +0200

    updates in code

--- Program.cs
+++ Program.cs
@@ -1,92 +1,101 @@
-﻿/*Objectives:
+﻿/*
 
-• Define an enumeration for the state of the chest.
-• Make a variable whose type is this new enumeration.
-• Write code to allow you to manipulate the chest with the lock, unlock, open, and close
-    commands, but ensure that you don’t transition between states that don’t support it.
-• Loop forever, asking for the next command.
 
-/*
+Define enumerations for the three variations on food: type (soup, stew, gumbo), main ingredient
+(mushrooms, chicken, carrots, potatoes), and seasoning (spicy, salty, sweet).
 
-/*
-The chest is locked. What do you want to do? unlock
-The chest is unlocked. What do you want to do? open
-The chest is open. What do you want to do? close
-The chest is unlocked. What do you want to do?
+
+Make a tuple variable to represent a soup composed of the three above enumeration types.
+
+
+Let the user pick a type, main ingredient, and seasoning from the allowed choices and fill the tuple
+with the results. Hint: You could give the user a menu to pick from or simply compare the user’s
+text input against specific strings to determine which enumeration value represents their choice.
+
+
+When done, display the contents of the soup tuple variable in a format like “Sweet Chicken Gumbo.”
+Hint: You don’t need to convert the enumeration value back to a string. Simply displaying an
+enumeration value with Write or WriteLine will display the name of the enumeration value.)
+
 
 */
 
 
+Console.WriteLine("What would be your order today?");
 
 
+var dishNameTuple = new Tuple<string, string, string>(
+   nameof(DishName.soup),
+   nameof(DishName.stew),
+   nameof(DishName.gumbo)
+);
 
 
-ChestState currentChest = ChestState.Locked;
+var dishMainIngredientTuple = new Tuple<string, string, string, string>(
 
-bool gameRunning = true;
 
-while (gameRunning)
+   nameof(DishMainIngredient.mushroom),
+   nameof(DishMainIngredient.chicken),
+   nameof(DishMainIngredient.carrot),
+   nameof(DishMainIngredient.potatoe)
+
+
+);
+
+
+var dishSeasoningTypeTuple = new Tuple<string, string, string>(
+   nameof(DishSeasoningType.spicy),
+   nameof(DishSeasoningType.salty),
+   nameof(DishSeasoningType.sweet)
+);
+
+
+Console.WriteLine($"We are offering {dishNameTuple} with the following main ingredients {dishMainIngredientTuple} and with the following seasonings {dishSeasoningTypeTuple}.");
+Console.WriteLine("Make your choice please.");
+
+
+string userInput = Console.ReadLine();
+string [] userInputParts = userInput.Split(' ');
+
+
+if (userInputParts.Length == 3 &&
+   Enum.TryParse<DishSeasoningType>(userInputParts[0], true, out var seasoning) &&         //how does this TryParse exactly work?
+   Enum.TryParse<DishMainIngredient>(userInputParts[1], true, out var ingredient) &&
+   Enum.TryParse<DishName>(userInputParts[2], true, out var dish))
+
+
 {
-    
-    Console.WriteLine($"The chest is currently {currentChest}. What would you like to do with it?");
-
-    string? playerInput = Console.ReadLine();
-
-    switch (playerInput)
-    {
-        case "exit":
-            gameRunning = false;
-            break;
-        case "unlock":
-            if(currentChest == ChestState.Locked)
-            {
-                currentChest = ChestState.Closed;
-            }
-            else 
-            {    
-                Console.WriteLine($"The chest is {currentChest}");
-            }
-            break;
-        case "open":
-            if(currentChest == ChestState.Closed)
-            {
-                currentChest = ChestState.Open;
-            }
-            else
-            {
-                Console.WriteLine($"The chest is {currentChest}");
-            }
-            break;
-        case "close":
-            if(currentChest == ChestState.Open)
-            {
-                currentChest = ChestState.Closed;
-            }
-            else
-            {
-                Console.WriteLine($"The chest is {currentChest}");
-            }
-            break;
-        case "lock":
-            if(currentChest == ChestState.Closed)
-            {
-                currentChest = ChestState.Locked;
-            }
-            else
-            {
-                Console.WriteLine($"The chest is {currentChest}");
-            }
-            break;     
-
-    }
- 
+   var
 }
 
-enum ChestState
+
+
+
+//var orderedDishTuple = (DishSeasoningType.spicy, DishMainIngredient.mushroom, DishName.soup);
+
+
+//Console.WriteLine("Here is your dish: " + orderedDishTuple);
+enum DishName
 {
-    Open,
-    Closed,
-    Locked,
-    Unlocked
-    
+   soup,
+   stew,
+   gumbo
 }
+
+
+enum DishMainIngredient
+{
+   mushroom,
+   chicken,
+   carrot,
+   potatoe
+}
+
+
+enum DishSeasoningType
+{
+   spicy,
+   salty,
+   sweet
+}
+
